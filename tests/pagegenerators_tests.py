d8782a5ccb008212fcc4b4ff889853bd99742e37
darthbhyrava <hbhyrava@gmail.com>
2016-03-11 00:30:32 +0530

    Change: Reimplement imageuncat's -recentchanges as a pagegenerators CL arg
    Modify pagegenerators' -recentchanges to add in imageuncat's corresponding param functionality.
    
    Bug: T129193
    Change-Id: I86251d22e9ef0e376e780093e9e53d69da7b6e0e

--- tests/pagegenerators_tests.py
+++ tests/pagegenerators_tests.py
@@ -803,6 +803,13 @@ class TestFactoryGenerator(DefaultSiteTestCase):
         self.assertIsNotNone(gen)
         self.assertPagesInNamespaces(gen, 1)
 
+    def test_recentchanges_timespan(self):
+        """Test recentchanges generator with delay and block params."""
+        gf = pagegenerators.GeneratorFactory(site=self.site)
+        gf.handleArg('-recentchanges:120,70')
+        gen = gf.getCombinedGenerator()
+        self.assertIsNotNone(gen)
+
     def test_recentchanges_ns_default(self):
         """Test recentchanges generator."""
         gf = pagegenerators.GeneratorFactory(site=self.site)
